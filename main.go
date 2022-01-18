3075bfdbfac79062647a0e333da674d598592f9e
Nidhey Indurkar <nidhey_indurkar@persistent.com>
2022-01-18 14:43:09 +0530

    first commit

--- main.go
+++ main.go
@@ -38,43 +38,43 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(Users)
 }
 
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    key := vars["id"]
+// func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+//     vars := mux.Vars(r)
+//     key := vars["id"]
 
-    for _, article := range Articles {
-        if article.Id == key {
-            json.NewEncoder(w).Encode(article)
-        }
-    }
-}
+//     for _, article := range Users {
+//         if user.Id == key {
+//             json.NewEncoder(w).Encode(user)
+//         }
+//     }
+// }
 
 
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
-    // get the body of our POST request
-    // unmarshal this into a new Article struct
-    // append this to our Articles array.    
-    reqBody, _ := ioutil.ReadAll(r.Body)
-    var article Article 
-    json.Unmarshal(reqBody, &article)
-    // update our global Articles array to include
-    // our new Article
-    Articles = append(Articles, article)
+// func createNewArticle(w http.ResponseWriter, r *http.Request) {
+//     // get the body of our POST request
+//     // unmarshal this into a new Article struct
+//     // append this to our Articles array.    
+//     reqBody, _ := ioutil.ReadAll(r.Body)
+//     var article Article 
+//     json.Unmarshal(reqBody, &article)
+//     // update our global Articles array to include
+//     // our new Article
+//     Articles = append(Articles, article)
 
-    json.NewEncoder(w).Encode(article)
-}
+//     json.NewEncoder(w).Encode(article)
+// }
 
-func deleteArticle(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id := vars["id"]
+// func deleteArticle(w http.ResponseWriter, r *http.Request) {
+//     vars := mux.Vars(r)
+//     id := vars["id"]
 
-    for index, article := range Articles {
-        if article.Id == id {
-            Articles = append(Articles[:index], Articles[index+1:]...)
-        }
-    }
+//     for index, article := range Articles {
+//         if article.Id == id {
+//             Articles = append(Articles[:index], Articles[index+1:]...)
+//         }
+//     }
 
-}
+// }
 
 func handleRequests() {
     myRouter := mux.NewRouter().StrictSlash(true)
